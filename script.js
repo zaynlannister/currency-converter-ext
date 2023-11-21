cbb7650dcd5b622f0bf58f66cd0313946207c38c
zaynlannister <zaynlannister@gmail.com>
2023-11-21 12:30:48 +0500

    feat: fetch currency & convert

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
-const currencyExchanged = document.querySelector(".currency-answ");
+const currencyExchanged = document.querySelector(".currency-answ input");
 const currencyValue = document.querySelector(".currency-value");
 const selectFrom = document.querySelector(".currency-from select");
 const selectTo = document.querySelector(".currency-to select");
 
 let value = null;
-let fromCurrency = null;
-let toCurrency = null;
+let fromCurrency = "";
+let toCurrency = "";
+let currentCurrency = null;
+
+const setInputValue = async () => {
+  const API = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${fromCurrency}/${toCurrency}.json`;
+  if (fromCurrency && toCurrency && !isNaN(parseInt(currencyValue.value))) {
+    await fetch(API)
+      .then((res) => res.json())
+      .then(async (res) => {
+        currentCurrency = res[toCurrency];
+      });
+    currencyExchanged.value = exchange(
+      currencyValue.value,
+      currentCurrency
+    ).toFixed(2);
+  } else {
+    currencyExchanged.value = "Choose currency!";
+  }
+};
+
+const exchange = (value, to) => {
+  return parseInt(value) * to;
+};
 
 const handleInputValue = (e) => {
   value = e.target.value;
-  console.log(value);
+  setInputValue();
 };
 
 const handleChangeFrom = (e) => {
   fromCurrency = e.target.value;
+  setInputValue();
 };
 
 const handleChangeTo = (e) => {
   toCurrency = e.target.value;
+  setInputValue();
 };
 
 currencyValue.addEventListener("input", handleInputValue);
